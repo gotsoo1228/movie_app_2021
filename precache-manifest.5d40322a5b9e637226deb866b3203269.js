d8bc04bb64b9e46b35b9fc073940d173c61a00b4
Munkhbaatar Tsog <tsogoo1228@gmail.com>
2020-07-11 22:34:43 +0900

    Updates

---
+++ precache-manifest.5d40322a5b9e637226deb866b3203269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0d0701d7c6cb491362206bbbc57609",
+    "url": "/movie_app_2021/index.html"
+  },
+  {
+    "revision": "cbd650cc220ba28ea542",
+    "url": "/movie_app_2021/static/css/main.d76085cb.chunk.css"
+  },
+  {
+    "revision": "7f8fe9dad69b38ccdb72",
+    "url": "/movie_app_2021/static/js/2.7e4496f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2021/static/js/2.7e4496f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd650cc220ba28ea542",
+    "url": "/movie_app_2021/static/js/main.89b5d167.chunk.js"
+  },
+  {
+    "revision": "4c9b4f0544d13b330fc4",
+    "url": "/movie_app_2021/static/js/runtime-main.ddc9a99e.js"
+  }
+]);
